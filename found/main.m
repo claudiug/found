3ea646a685748c7a211353bd83dddd6e85df9dd3
claudiu <claudiu.garba@gmail.com>
2014-02-01 19:33:57 +0100

    some alloc

--- found/main.m
+++ found/main.m
@@ -16,10 +16,15 @@ int main(int argc, const char * argv[])
         // insert code here...
         NSLog(@"Hello, World!");
         NSDate *now = [NSDate date];
-        
         NSLog(@"this obj at %@", now);
+        NSHost *hostType = [NSHost currentHost];
+        NSLog(@"hostname is %@", [hostType localizedName]);
+        NSDate *time = [NSDate date];
+        NSLog(@"time in future: %@", [time dateByAddingTimeInterval:10000]);
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSLog(@"calendar; %@", [cal calendarIdentifier]);
+        NSLog(@"calendar; %lu", (unsigned long)[cal ordinalityOfUnit:NSDayCalendarUnit inUnit:NSMonthCalendarUnit forDate:time]);
 
-        
     }
     return 0;
 }
