8f4b609af5f09298c1e9160c2d97bb231adb0fe8
claudiu <claudiu.garba@gmail.com>
2014-02-01 18:33:28 +0100

    init

---
+++ found/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  found
+//
+//  Created by claudiu on 2/1/14.
+//  Copyright (c) 2014 claudiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
